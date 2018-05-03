5406b6baf4f913f94cf7b5ed090afd54f8eb8911
rakesh2cp <rakesh.cp@gmail.com>
2018-05-03 16:05:30 +0800

    Update folder_organize.py

--- folder_organize.py
+++ folder_organize.py
@@ -65,3 +65,40 @@ def main():
 			#print "split file name", split_name
 
 main()
+
+#import math, time
+#
+## Set listing start location
+##start_path = "C:\\Users\\1578413\\Downloads\\"
+#start_path = "C:\\Users\\1578413\\Desktop\\"
+#dir_count = 0
+#file_count = 0
+#
+## Traverse directory tree
+#for (path, dirs, files) in os.walk(start_path):
+#    #print('Directory: {:s}'.format(path))
+#    dir_count += 1
+#    # Repeat for each file in directory
+#    for file in files:
+#        fstat = os.stat(os.path.join(path, file))
+#
+#        # Convert file size to MB, KB or Bytes
+#        if (fstat.st_size > 1024 * 1024):
+#            fsize = math.ceil(fstat.st_size / (1024 * 1024))
+#            unit = "MB"
+#            print(path, '\t{:15.15s}{:8d} {:2s} {:18s}'.format(file, fsize, unit, mtime))
+#        elif (fstat.st_size > 1024):
+#            fsize = math.ceil(fstat.st_size / 1024)
+#            unit = "KB"
+#        else:
+#            fsize = fstat.st_size
+#            unit = "B"
+#
+#        mtime = time.strftime("%X %x", time.gmtime(fstat.st_mtime))
+#
+#        # Print file attributes
+#        #if (fsize > 2 * 1024 * 1024):
+#        #print('\t{:15.15s}{:8d} {:2s} {:18s}'.format(file, fsize, unit, mtime))
+#        file_count += 1
+## Print total files and directory count
+#print('\nFound {} files in {} directories.'.format(file_count, dir_count))
